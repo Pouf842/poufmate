a5bd4b21e90aa627548676393470e2199e619865
Pouf842 <Pouf842@834ba0b8-e4c2-11de-af1a-fd75af6c8208>
2011-04-04 15:06:05 +0000

    Test Hangover

--- branches/KBang/src/server/cardhangover.cpp
+++ branches/KBang/src/server/cardhangover.cpp
@@ -1,4 +1,7 @@
 #include "cardhangover.h"
+#include "player.h"
+#include "game.h"
+#include "gamecycle.h"
 
 CardHangOver::CardHangOver(Game * game, int id) : HighNoonCard(game, id, HIGHNOON_HANGOVER)
 {
@@ -10,4 +13,19 @@ CardHangOver::~CardHangOver()
 
 void CardHangOver::play()
 {
+    mp_game->gameCycle().registerActionChecker(this);
+    connect(&mp_game->gameCycle(), SIGNAL(newGameTurn()),
+            this, SLOT(stop()));
+}
+
+void CardHangOver::stop()
+{
+    mp_game->gameCycle().unregisterActionChecker(this);
+    disconnect(&mp_game->gameCycle(), SIGNAL(newGameTurn()),
+            this, SLOT(stop()));
+}
+
+bool CardHangOver::checkAbility(Player *)
+{
+    return false;
 }
