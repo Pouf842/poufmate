349776da124602d8690e3a09822b4abfa60b7b57
Pouf842@gmail.com <Pouf842@gmail.com@834ba0b8-e4c2-11de-af1a-fd75af6c8208>
2011-07-30 18:38:37 +0000

    Gold rush begun.

--- branches/KBang/src/server/cardgoldrush.cpp
+++ branches/KBang/src/server/cardgoldrush.cpp
@@ -1,4 +1,6 @@
 #include "cardgoldrush.h"
+#include "gamecycle.h"
+#include "game.h"
 
 CardGoldRush::CardGoldRush(Game * game, int id) : HighNoonCard(game, id, HIGHNOON_GOLD_RUSH)
 {
@@ -10,4 +12,16 @@ CardGoldRush::~CardGoldRush()
 
 void CardGoldRush::play()
 {
+	connect(&mp_game->gameCycle(), SIGNAL(newGameTurn()),
+			this, SLOT(stop()));
+
+	QList<Player*> players = mp_game->playerList();
+
+
+}
+
+void CardGoldRush::stop()
+{
+	disconnect(&mp_game->gameCycle(), SIGNAL(newGameTurn()),
+			   this, SLOT(stop()));
 }
